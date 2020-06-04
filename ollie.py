7515b31103e9e0cd66f833e65d5c64c7251d428d
oladolapo <66223952+oladolapo@users.noreply.github.com>
2020-06-04 07:38:09 +0100

    Update ollie.py

--- ollie.py
+++ ollie.py
@@ -1,11 +1,8 @@
 def introduction(name, ID, language, Email):
     result = "Hello world, this is {} HNGi7 {}, using {} for stage 2 task. {}."
     return (result.format(name, ID, language, Email))
-<<<<<<< HEAD
 
 
-=======
->>>>>>> 61542d05c7ac7f4a87893d4ec83b544c72ba27cc
 message = introduction("Ogunsipe Oladolapo", "HNG-00590",
                        "python", "dolapoogunsipe@gmail.com")
 print(message)
